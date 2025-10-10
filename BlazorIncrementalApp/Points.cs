4f9522047e5fce7c26d05266a894fa585ba8866d
Ben4311956 <benceglokler@gmail.com>
2025-10-10 17:00:29 +0100

    Add project files.

---
+++ BlazorIncrementalApp/Points.cs
@@ -0,0 +1,18 @@
+﻿public class Points
+{
+    public event Action? OnChange;
+
+    private int _point;
+    public int Point 
+    { 
+        get => _point; 
+        set 
+        {
+            _point = value;
+            NotifyStateChanged();
+        } 
+    }
+    public int Upgrades { get; set; }
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
